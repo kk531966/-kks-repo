c2e21b353516535c6fea1c45d1e8f43596e73ce0
kiran kakde <kakdekiranp@gmail.com>
2022-04-06 22:30:59 +0530

    demo

--- src/practice/Test_demo.java
+++ src/practice/Test_demo.java
@@ -5,6 +5,22 @@ public class Test_demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int no=615;
+		int temp=no;
+		int rev=0,rem;
+		
+		while (temp!=0)
+		{
+			rem=temp%10;
+			rev=rev*10+rem;
+			temp=temp/10;
+		}
+	    if (no==rev) {
+	    	System.out.println(no+"is palindrom number");
+	    }
+	    else {
+	    	System.out.println(no+"is not palindrom number");
+	    }
 	}
 
 }
